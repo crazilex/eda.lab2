513b566a1222b40f2808a5dd3e6b98d64dce34f8
theTendo <urtzigl@gmail.com>
2017-10-26 16:13:03 +0200

    cambio de nombre BaseDatos -> Diccionario

--- actividad1/codigo/Diccionario.java
+++ actividad1/codigo/Diccionario.java
@@ -7,28 +7,28 @@ import java.util.HashMap;
 //import java.util.Set;
 
 
-public class BaseDatos {
+public class Diccionario {
 	//Atributos
-	private static BaseDatos miBaseDatos = null;
+	private static Diccionario miDiccionario = null;
 	private HashMap<String, ArrayList<String>> palabras = null;
-	
+
 	//Constructora
-	private BaseDatos(){
+	private Diccionario(){
 		this.palabras = new HashMap<String, ArrayList<String>>();
 	}
-	
-	public static BaseDatos getBaseDatos() {
-		if(miBaseDatos == null) {
-			miBaseDatos = new BaseDatos();
+
+	public static Diccionario getDiccionario() {
+		if(miDiccionario == null) {
+			miDiccionario = new Diccionario();
 		}
-		return miBaseDatos;
+		return miDiccionario;
 	}
-	
+
 	//Devuelve el hashmap.
 	public HashMap<String, ArrayList<String>> getPalabras(){
 		return this.palabras;
 	}
-	
+
 	//Esto sera para la constructora, para poder añadir las palabras al diccionario.
 	//pre: Una palabra del diccionario no vacia.
 	//post: Annade esa palabra como key en caso de no existir.
@@ -38,7 +38,7 @@ public class BaseDatos {
 			this.palabras.put(palabra, l);
 		}
 	}
-	
+
 	//Metodo que se utilizara para introducir el nombre de una Web en las palabras del Diccionario.
 	//pre: Un nombre no vacio de una web.
 	//post: Annade el nombre de la web a todas las palabras que utiliza esa web.
@@ -54,9 +54,9 @@ public class BaseDatos {
 	        	}
 	        }
 		}
-		
+
 	}
-	
+
 	//Metodo que se utilizara para eliminar el nombre de una Web en las palabras del Diccionario.
 	//pre: Un nombre no vacio de una web.
 	//post: Elimina el nombre de la web en todas las palabras que utiliza esa web.
@@ -73,14 +73,14 @@ public class BaseDatos {
 			}
 		}
 	}
-	
+
 	//pre: Recibe lo que ha sido escrito por teclado.
 	//post: Devuelve la lista de paginas relacionadas con la palabra.
 	public void buscarWeb(String[] busca){
 		boolean sinCoincidencias = true;
 		for (int i = 0; i< busca.length; i++){
 			for (int l = 4; l<=busca[i].length(); l++){
-				for (int pos = 0; pos<=(busca[i].length()-l); pos++) {	
+				for (int pos = 0; pos<=(busca[i].length()-l); pos++) {
 					String posible = busca[i].substring(pos, pos+l);
 					if(palabras.containsKey(posible)){
 						System.out.println("PARA: "+ posible);
@@ -95,9 +95,9 @@ public class BaseDatos {
 			System.out.println("No se han encontrado ninguna coincidencia");
 		}
 	}
-	
-	
-	
+
+
+
 	//Imprime todas las keys y sus listas.
 	public void imprimir(){
 	    for(String key: palabras.keySet()){
@@ -105,7 +105,7 @@ public class BaseDatos {
 	    }
 
 	}
-	
+
 	//Vacia el hashmap.
 	public void reset(){
 		this.palabras.clear();
