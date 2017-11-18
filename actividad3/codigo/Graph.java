9111c8d029ab142d6bfc22c3559aafae0b165997
Alex Ye <ayxworks@gmail.com>
2017-11-18 12:12:46 +0100

    terminar estanConectados
    cambios varios sigue mal la inicializacion cambiando

--- actividad3/codigo/Graph.java
+++ actividad3/codigo/Graph.java
@@ -15,31 +15,23 @@ public class Graph {
 		//       Los nodos son nombres de webs
 
 		// Paso 1: llenar “th”
+
+		int tamanoArbol = webs.tamano();
 		PagWeb web;
+		adjList = new ArrayList<Integer>()[web.+1000]; //el array tiene espacio para todas las webs del arbol y otras 1.000 mas
 		Iterator<PagWeb> it = webs.getIterador();
 		while (it.hasNext()){
 			web = it.next();
 			String nombre = web.getNombre();
 			int id = web.getId();
 			th.put(nombre,id);
+			// Paso 3: llenar “adjList”
+			adjList[id] = web.getListaRef(); //anade en el array de referencias en la posicion segun el id de la web
 		}
 
-		// Paso 2: llenar “keys”
+		// Paso 2: llenar “keys” wn un array
 		keys = new String[th.size()];
 		for (String k: th.keySet()) keys[th.get(k)] = k;
-
-		// Paso 3: llenar “adjList”
-		PagWeb webAux;
-		int webID;
-		int tamanoArbol = webs.tamano();
-		adjList = new ArrayList<Integer>()[tamanoArbol+1000]; //el array tiene espacio para todas las webs del arbol y otras 1.000 mas
-
-		Iterator<PagWeb> it2 = webs.getIterador();
-		while (it2.hasNext()){
-			web = it2.next();
-			webID = web.getId();
-			adjList[webID] = web.getListaRef(); //anade en el array de referencias en la posicion segun el id de la web
-		}
 
 	}
 
@@ -51,19 +43,30 @@ public class Graph {
 		System.out.println();
 	   }
 	}
-	/*
+	
 	public boolean estanConectados(String a1, String a2){
 		Queue<Integer> porExaminar = new LinkedList<Integer>();
-
+		
 		int pos1 = th.get(a1);
 		int pos2 = th.get(a2);
 		boolean enc = false;
 		boolean[] examinados = new boolean[th.size()];
 
-                 // COMPLETAR CÓDIGO
-
+            // COMPLETAR CÓDIGO
+			if(a1.equals(a2)) enc = true;			
+			else{
+				examinados[pos1] = true;
+				porExaminar.add(pos1);
+				while(!enc && porExaminar.isEmpty()){
+					int actual = porExaminar.poll();
+					examinados[actual] = true;
+					if(actual == pos2) enc = true;	
+					else{
+						for (int i = 0; i<adjList[actual].size();i++){
+							porExaminar.add([actual].get(i));
+						}
+					}
+				}
+			}
 		return enc;
-
-	}
-	*/
 }
